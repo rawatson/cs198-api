3adb84862e82b0e9a3cc0b68f9ee059f4c043fe4
Omar Diab <o.s.diab@gmail.com>
2014-10-14 00:47:50 -0700

    allow nil citizen type

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,5 +3,5 @@ class Person < ActiveRecord::Base
 
   validates :suid, uniqueness: true, allow_nil: true
   validates :sunet_id, uniqueness: true
-  validates :citizen_status, inclusion: { in: @citizen_types }
+  validates :citizen_status, inclusion: { in: @citizen_types }, allow_nil: true
 end
