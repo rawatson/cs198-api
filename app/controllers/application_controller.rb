af086ce80a3171ff123479598a3e5226af337148
Omar Diab <o.s.diab@gmail.com>
2014-11-03 18:33:35 -0800

    add hook to convert boolean parameters for all routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :convert_boolean_params
+
+  private
+
+  def convert_boolean_params
+    params.map do |k, v|
+      case v
+      when "true"
+        params[k] = true
+      when "false"
+        params[k] = false
+      end
+    end
+  end
 end
