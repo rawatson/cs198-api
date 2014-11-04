062f5de692d53efc3f3a471cacd3540d56e78825
Omar Diab <o.s.diab@gmail.com>
2014-11-03 19:49:06 -0800

    stubs for lair/helperscontroller

---
+++ app/controllers/lair/helpers_controller.rb
@@ -0,0 +1,25 @@
+class Lair::HelpersController < ApplicationController
+  def index
+    fail "Not implemented"
+  end
+
+  def create
+    fail "Not implemented"
+  end
+
+  def shifts
+    fail "Not implemented"
+  end
+
+  def show
+    fail "Not implemented"
+  end
+
+  def update
+    fail "Not implemented"
+  end
+
+  def destroy
+    fail "Not implemented"
+  end
+end
---
+++ test/controllers/lair/helpers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+describe Lair::HelpersController do
+  it "must be a real test" do
+    flunk "Need real tests"
+  end
+end
