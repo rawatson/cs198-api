fd3738224cc30e295130da349d2b840f70fb2d77
Omar Diab <o.s.diab@gmail.com>
2014-11-08 14:34:51 -0800

    add validations to help requests

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -2,4 +2,17 @@ class HelpRequest < ActiveRecord::Base
   belongs_to :enrollment
   has_one :person, through: :enrollments
   has_one :course, through: :enrollments
+
+  validates :enrollment, uniqueness: {
+    conditions: -> { where open: true },
+    if: -> (hr) { hr.open },
+    message: "only one open help request per enrollment is allowed"
+  }, presence: true
+  validate :validate_enrollment
+
+  def validate_enrollment
+    errors.add(:enrollment, "person must be enrolled as a student " \
+                            "to request help") unless enrollment.position == "student"
+    # TODO: enforce currently active term
+  end
 end
