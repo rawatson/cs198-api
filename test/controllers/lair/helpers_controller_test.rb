5e745e2a822419b188a0a8e47fe55ab3e00a6b57
Omar Diab <o.s.diab@gmail.com>
2014-11-08 17:20:57 -0800

    add create controller unit tests

--- test/controllers/lair/helpers_controller_test.rb
+++ test/controllers/lair/helpers_controller_test.rb
@@ -47,4 +47,39 @@ describe Lair::HelpersController do
       data.each { |p| p[:checked_out].wont_be_nil } # TODO: improve HelperCheckin format check
     end
   end
+
+  describe :create do
+    it "must 404 on nonexistent person" do
+      post :create, person: "hello"
+
+      assert_response :missing
+
+      message = JSON.parse(@response.body, symbolize_names: true)[:message]
+      message.must_equal "Person not found"
+    end
+
+    it "must 201 on a new checkin" do
+      post :create, person: people(:staff_4).id
+      assert_response :created
+
+      data = JSON.parse(@response.body, symbolize_names: true)[:data]
+      data[:person_id].must_equal people(:staff_4).id
+    end
+
+    it "must 200 on an already existent checkin" do
+      post :create, person: people(:staff_1).id
+      assert_response :ok
+
+      data = JSON.parse(@response.body, symbolize_names: true)[:data]
+      data[:person_id].must_equal people(:staff_1).id
+    end
+
+    it "must 403 on a non-staff person" do
+      post :create, person: people(:student_1).id
+      assert_response :forbidden
+
+      message = JSON.parse(@response.body, symbolize_names: true)[:message]
+      message.must_equal "Must be an active staff member to check in as a helper."
+    end
+  end
 end
