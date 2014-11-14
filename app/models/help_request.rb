90d21a2da1c1e63e8b6aabab2c3ece7a4af1d1f1
Omar Diab <o.s.diab@gmail.com>
2014-11-13 20:40:50 -0800

    fix enrollments plurality in HelpRequests model

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,7 +1,7 @@
 class HelpRequest < ActiveRecord::Base
   belongs_to :enrollment
-  has_one :person, through: :enrollments
-  has_one :course, through: :enrollments
+  has_one :person, through: :enrollment
+  has_one :course, through: :enrollment
 
   validates :enrollment, uniqueness: {
     conditions: -> { where open: true },
