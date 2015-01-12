5205c011a7d1e8b7319c066f64214f09a19f3566
Omar Diab <o.s.diab@gmail.com>
2015-01-12 01:53:31 -0800

    string genders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201090945) do
+ActiveRecord::Schema.define(version: 20150112004752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 20141201090945) do
     t.string   "nick_name"
     t.string   "email",          null: false
     t.string   "phone_number"
-    t.boolean  "gender"
+    t.string   "gender"
     t.boolean  "scpd"
     t.boolean  "staff",          null: false
     t.boolean  "active"
