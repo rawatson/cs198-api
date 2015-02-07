81155c5b911c36fd33925b220f2aff6de06937d5
Omar Diab <o.s.diab@gmail.com>
2015-02-06 23:22:13 -0800

    don't forget dependency

--- app/helpers/save_helper.rb
+++ app/helpers/save_helper.rb
@@ -1,5 +1,6 @@
 # Helpers for saving models.
 module SaveHelper
+  include Errors
   def save_multiple(instances)
     instances.values.each { |i| i.save validate: false }
     return unless instances.values.map(&:valid?).include? false
