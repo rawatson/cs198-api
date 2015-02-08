c30c9b81effdda66892493c0b5f8e8d4f4e7c5cf
Omar Diab <o.s.diab@gmail.com>
2015-02-07 16:07:18 -0800

    don't include valid records in recordsnotvalid

--- app/helpers/save_helper.rb
+++ app/helpers/save_helper.rb
@@ -6,9 +6,11 @@ module SaveHelper
       if records.is_a? Hash
         records.each { |_, i| i.save validate: false }
         return unless records.map { |_, i| i.valid? }.include? false
+        records = records.reject { |_, i| i.valid? }
       elsif records.is_a? Array
         records.each { |i| i.save validate: false }
         return unless records.map(&:valid?).include? false
+        records = records.reject(&:valid?)
       else
         fail "records must be a collection of records"
       end
